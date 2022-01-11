3f6986b1d529e6e6387d8d85e6ad855835115ee3
yashkumartiwari <kumaryash6249@gmail.com>
2022-01-11 23:31:33 +0530

    prod datasey url changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  colorApi_URL: "https://color-names.herokuapp.com/v1/"
+  // colorApi_URL: "https://color-names.herokuapp.com/v1/"
+  colorApi_URL: "https://api.color.pizza/v1/"
 };
